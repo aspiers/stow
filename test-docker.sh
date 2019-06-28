ee57b1abc60844c1c5bacb8e92b5a28fdb189fc6
Adam Spiers <stow@adamspiers.org>
2019-06-28 17:08:48 +0100

    Make docker scripts use get-version

--- test-docker.sh
+++ test-docker.sh
@@ -3,6 +3,6 @@
 # Test Stow across multiple Perl versions, by executing the
 # Docker image built via build-docker.sh.
 
-version=$( git describe --match v* --abbrev=0 )
+version=$( tools/get-version )
 
 docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) stowtest:$version
